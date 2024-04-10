2a6bd01bac21cb42568b73ed9ccd351521bd52eb
Anchu Antony <anchuantony2212@gmail.com>
2024-04-10 14:28:56 +0200

    new files

---
+++ table.js
@@ -0,0 +1,12 @@
+const companies = [
+    ["Saab AB", "Stockholm", 1937],
+    ["Volvo Group", "Göteborg", 1927],
+    ["Sandvik AB", "Stockholm", 1862],
+    ["Atlas Copco Group", "Nacka", 1873],
+    ["Electrolux AB", "Stockholm", 1919],
+    ["Skanska AB", "Stockholm", 1887],
+];
+
+
+
+
